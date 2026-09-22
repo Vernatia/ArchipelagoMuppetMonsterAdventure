d033f0e98357ecf1751f64bd4d0440d5774e938f
Vernatia <vernatia_1111@hotmail.com>
2026-09-22 21:53:07 +1000

    fix: Bad merge

--- worlds/muppet_monster_adventure/shared.py
+++ worlds/muppet_monster_adventure/shared.py
@@ -22,11 +22,13 @@ class LevelName(StrEnum):
     POKER_FACES = "Poker Faces"
     NOSEFERATU = "Noseferatu"
 
+
 whitelisted_starting_levels: list[LevelName] = [
     LevelName.PEACOCK_PURGATORY,
     LevelName.HALLWAYS_OF_DOOM,
     LevelName.POKER_FACES,
 ]
+
 
 class ItemFlag(Flag):
     GAIN_HEALTH = auto(),
@@ -39,4 +41,3 @@ class TrapFlag(Flag):
     LOSE_HEALTH = auto(),
     LOSE_HEART  = auto(),
     LOSE_LIFE   = auto(),
-]
